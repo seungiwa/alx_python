cdb9eb911733b60b304ad9944e8f063ef81f2fe2
seungiwa <seungiwa8@gmail.com>
2023-07-19 21:15:00 +0100

    Added changes

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for word in range(100):
-    # Print two digits with leading zeros if necessary
-    print('{:02}'.format(word), end=", ")
-    if word == 99:
-        print(word, end="\n ")
+for num in range(100):
+        #end = ", " if num is less than 99
+        print("{:02}".format(num), end=", " if num < 99 else "\n")
+
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,5 @@
 for word in range(0, 9):
     #Iterating through all possible combinations of two digits, excluding duplicates like "01" and "10."
     for word1 in range(word + 1, 10):
-        print("{}{}".format(word, word1), end=", ")
-        if (word == 8) & (word1 == 9):
-            print('{}{}'.format(word, word1), end="\n")
+        print("{}{}".format(word, word1), end=", " if (word < 8) else "\n")
+        
